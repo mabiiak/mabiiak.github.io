1d57963df77c82d44c0d87a2707b7aa0de78e29f
mabiane <mabiane.polniak@gmail.com>
2022-02-06 16:59:25 -0300

    adciona header

---
+++ react-portifolio/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { HeaderStyled } from '../Styled';
+
+export default function Header() {
+  return (
+    <HeaderStyled>
+      <p>Mabiane Polniak</p>
+      <div>
+        <p> Sobre </p>
+        <p> Projetos </p>
+        <p> Contato </p>
+      </div>
+
+    </HeaderStyled>
+  );
+}
---
+++ react-portifolio/src/components/Styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const HeaderStyled = styled.header `
+  background-color: #B0DBDB;
+  display: flexbox;
+  justify-content: space-between;
+  padding: 9px;
+  
+  p {
+    margin-left: 30px
+  }
+
+  div {
+    display: flexbox;
+    margin-top: 5px;
+    margin-right: 30px;
+    p {
+      margin: 10px;
+    }
+  }
+`
