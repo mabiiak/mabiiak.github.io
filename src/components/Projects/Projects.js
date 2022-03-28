323184e41a7ae83efd94b2d7a457bf45aad2bce9
mabiak <mabiane.polniak@gmail.com>
2022-03-28 18:21:48 -0300

    chore: adiciona projeto trybewallet ao carrossel

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import pato from '../../images/pato.jpeg';
 import { Project }  from '../Styled';
 
+import pato from '../../images/pato.jpeg';
 import starWars from '../../images/starwarsPlanets.png';
+import wallet from '../../images/trybewalet.png';
+import trybetunes from '../../images/trybetunes.png';
 
 export default function Projects() {
   return (
@@ -12,7 +14,7 @@ export default function Projects() {
       <h1>Projetos</h1>
       <Carousel className='car'>
         <div>
-          <img src={starWars} alt="pato" width="600px"/>
+          <img src={starWars} alt="starWars" width="600px"/>
           <div className="legenda">
             <h3>Star Wars planets Search</h3>
             <p>
@@ -22,12 +24,16 @@ export default function Projects() {
             </p>
           </div>
         </div>
-        <div>
-          <img src={pato} alt="pato"/>
-          <p>Sobre2</p>
+        <div className="legenda">
+          <img src={wallet} alt="wallet"/>
+          <h3>Trybewallet</h3>
+          <p>
+            Projeto feito durante o curso de desenvolvimento Web na Trybe, no módulo de Front-End. Utilizando heact Redux e css.<br/><br/> Ele tinha como objetivo criar uma aplicação que seja capaz de armazenar e somar as despesas do usuario, levando em conta a moeda, a cotação da moeda no dia da compra.<br/><br/>. 
+            <a href="https://github.com/mabiiak/trybewallet">Link do repositório</a><br/>
+            <a href="https://mabiiak.github.io/trybewallet/">Link do projeto</a></p>
         </div>
         <div>
-          <img src={pato} alt="pato"/>
+          <img src={trybetunes} alt="trybetunes"/>
           <p>Sobre3</p>
         </div>
       </Carousel>
